2fa4c83d5b462388ee0fa2788935d5c500996566
jannabelramos <jannabelramos@gmail.com>
2022-04-27 01:52:04 -0400

    UsersController

--- FORFOOD/Controllers/ProductsController.cs
+++ FORFOOD/Controllers/ProductsController.cs
@@ -9,32 +9,31 @@ namespace FORFOOD.Controllers
 {
     public class ProductsController : Controller
     {
-
+        //Instancia de un ProductService
         private readonly ProductsService productService = new ProductsService(new ProductsRepository(new ForfoodContext()));
 
-        public ProductsController()
-        {
-
-        }
-
+        //Este método obtiene todos los productos registrados
         [HttpGet("GetProducts")]
         public async Task<JsonResult> GetProducts()
         {
             return Json(await productService.GetAll());
         }
 
+        //Este método crea un nuevo producto
         [HttpPost("CreateProducts")]
         public async Task<JsonResult> CreateProducts(Products product)
         {
             return Json(await productService.Insert(product));
         }
 
+        //Este método elimina un producto a partir de un ID
         [HttpDelete("DeleteProducts")]
         public async Task DeleteProducts(int product)
         {
             await productService.Delete(product);
         }
 
+        //Este método actualiza un producto (Toma como referencia el ID)
         [HttpPut("UpdateProducts")]
         public async Task UpdateProducts(Products product)
         {
---
+++ FORFOOD/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FORFOOD.Data;
+using FORFOOD.Services.Implements;
+using FORFOOD.Repositories.Implements;
+using FORFOOD.Models;
+
+namespace FORFOOD.Controllers
+{
+    public class UsersController : Controller
+    {
+        //Esto crea una nueva instancia de un UserService
+        private readonly UsersService usersService = new UsersService(new UsersRepository(new ForfoodContext()));
+
+        
+        //Este método obtiene todos los usuarios registrados.
+        [HttpGet("GetUsers")]
+        public async Task<JsonResult> GetUsers()
+        {
+            return Json(await usersService.GetAll());
+        }
+
+        //Este método elimina un usuario a partir de un ID
+        [HttpDelete("DeleteUsers")]
+        public async Task DeleteUsers(int id)
+        {
+            await usersService.Delete(id);
+        }
+
+        //Este método actualiza un usuario (Toma como referencia el ID)
+        [HttpPut("UpdateUsers")]
+        public async Task<JsonResult> UpdateUsers(User users)
+        {
+            return Json(await usersService.Update(users));
+        }
+        
+        //Este método crea un nuevo usuario.
+        [HttpGet("CreateUsers")]
+        public async Task<JsonResult> CreateUsers(User user)
+        {
+            return Json(await usersService.Insert(user));
+        }
+    }
+}
